9b7531089c96dad65d893e3062bbe4df827981a1
Hao Zhou <haozhou@192.168.1.56>
2026-06-27 00:54:09 +0200

    feat: proxy /pocketmoda to pocketmoda.vercel.app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/pocketmoda',
+        destination: 'https://pocketmoda.vercel.app/pocketmoda',
+      },
+      {
+        source: '/pocketmoda/:path*',
+        destination: 'https://pocketmoda.vercel.app/pocketmoda/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
